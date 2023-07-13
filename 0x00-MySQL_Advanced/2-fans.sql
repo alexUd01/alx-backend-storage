f20067079e1fa06c836d14205eb193160f400118
alexUd01 <alexanderikpeama@gmail.com>
2023-07-13 13:19:22 +0100

    Task 3

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -8,4 +8,6 @@ REQUIREMENTS:
 */
 
 -- The query
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
