4a1a2f83905f42079f3b2f6a3807aa7f391f05b8
alexUd01 <alexanderikpeama@gmail.com>
2023-07-18 21:42:42 +0100

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3
-""" A python script that provides sole stats about Nginx logs stored in MongoDB
+""" A python script that provides some stats about Nginx logs stored in MongoDB
 """
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from typing import List
+
+
+def connect_to_db(host: str = '127.0.0.1', port: int = 27017) -> Collection:
+    """ A function that establishes a connection to a MongoDB database """
+    client: MongoClient = MongoClient('mongodb://{:s}:{:d}'.format(host, port))
+    return client.logs.nginx
+
+
+def main() -> None:
+    """
+    A function that parses the data from the `nginx` collections
+    in the `logs` database
+    """
+    collection: Collection = connect_to_db()
+
+    print("{:d} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("    method GET: {:d}".format(
+        collection.count_documents({"method": "GET"})))
+    print("    method POST: {:d}".format(
+        collection.count_documents({"method": "POST"})))
+    print("    method PUT: {:d}".format(
+        collection.count_documents({"method": "PUT"})))
+    print("    method PATCH: {:d}".format(
+        collection.count_documents({"method": "PATCH"})))
+    print("    method DELETE: {:d}".format(
+        collection.count_documents({"method": "DELETE"})))
+    print("{:d} status check".format(
+        collection.count_documents({"path": "/status"})))
+
+
+if __name__ == "__main__":
+    main()
